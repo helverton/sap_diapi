362c3c092ff3a2917fa4714360e9a5ff6cdc1232
Helverton Santos <helvertonsantos@gmail.com>
2022-04-10 19:18:05 -0300

    Add files via upload

---
+++ Hangfire/HangFireController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Hangfire;
+
+namespace HelvertonSantos.Controllers
+{
+    public class HangFireController
+    {
+        public static void Start()
+        {
+            RecurringJob.AddOrUpdate<ZenviaJobsController>(x => x.SendSmsBoeExpire(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+            RecurringJob.AddOrUpdate<ZenviaJobsController>(x => x.SendSmsBoeExpired(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<ECommerceJobsController>(x => x.SendOrderStatus(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+            RecurringJob.AddOrUpdate<ECommerceJobsController>(x => x.SendStockStatus(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+            RecurringJob.AddOrUpdate<ECommerceJobsController>(x => x.SendInvoiceStatus(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<PipefyJobsController>(x => x.GetPartners(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<EadJobsController>(x => x.GetOrders(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<GServiceJobsController>(x => x.UpdateDataEAD(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<GServiceJobsController>(x => x.UpdateDataPgrMeOperations(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<SalesforceJobsController>(x => x.GetSForceToken(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+            RecurringJob.AddOrUpdate<SalesforceJobsController>(x => x.SyncAccount(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+            RecurringJob.AddOrUpdate<SalesforceJobsController>(x => x.SyncOpportunity(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<DootaxJobsController>(x => x.Request(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<SapRpaJobsController>(x => x.PrchRtrnCompany1_To_OrdrRtrnCompany2(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+
+            RecurringJob.AddOrUpdate<PepperiJobsController>(x => x.GetOrders(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+            RecurringJob.AddOrUpdate<PepperiJobsController>(x => x.UpdateOrders(), "* 7 * * 1-5", TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+        }
+    }
+}
---
+++ Hangfire/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Owin;
+using Hangfire;
+using Microsoft.Owin;
+using System.Globalization;
+using HelvertonSantos.Controllers;
+
+[assembly: OwinStartup(typeof(HelvertonSantos.Startup))]
+
+namespace HelvertonSantos
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string conf = $"Server={System.Configuration.ConfigurationManager.AppSettings["Server"]}; " +
+                          $"Database ={ System.Configuration.ConfigurationManager.AppSettings["Database"]}; " +
+                          $"User Id = { System.Configuration.ConfigurationManager.AppSettings["User"] }; " +
+                          $"Password ={ System.Configuration.ConfigurationManager.AppSettings["Password"]}; ";
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage(conf);
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            HangFireController.Start();
+        }
+    }
+}
